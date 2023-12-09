2542040a6f6caedc67b350ef5e796bf8a213d73c
Suvarna_Narayanan <suvarnanarayanan2g@gmail.com>
2023-12-09 21:35:52 +0530

    array_jump

--- algo&ds/top150InterViewQs/arrays/jump_game_v2/Main.java
+++ algo&ds/top150InterViewQs/arrays/jump_game_v2/Main.java
@@ -1,3 +1,36 @@
+import java.util.Arrays;
+
+/**
+ *
+ * QUESTION:
+ *
+ * You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+ *
+ * Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+ *
+ * 0 <= j <= nums[i] and
+ * i + j < n
+ *
+ * Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+ *
+ *
+ */
+
+
+
+/**
+ * EXPLANATION:
+ *
+ * To be precise, the first loop changes the values of the array so that index N becomes the maximum index to which we can jump if we are at the most “favorable” index from the index segment [M + 1, N], where M determines the last index the previous segment.
+ *
+ * nums[i-1] already contains maximum we can reach upto from i-1
+ * for the ith element, we can go further if nums[i] + i > (modified)nums[i-1]
+ * if not, we are better off by jumping from i-1 itself, and never reaching i
+ *
+ * The second loop simply accumlates the count of these maximised jumps.
+ *
+ */
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,23 +38,19 @@ public class Main {
         System.out.println(jump(numbers));
     }
 
+    // leetcode's best solution
     public static int jump(int[] nums) {
-        int numbersToJump = nums.length-1;
-        int jumpCount = 0;
-        if(nums.length == 1) {
-            return jumpCount;
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i]+i, nums[i-1]);
         }
-        int i = 0;
-        while(i < nums.length) {
-            if(nums[i] >= numbersToJump) {
-                return ++jumpCount;
-            } else {
-                int temp = nums[i];
-                i = nums[i];
-                jumpCount++;
-                numbersToJump = numbersToJump - temp;
-            }
+        int ind = 0;
+        int ans = 0;
+
+        while(ind < nums.length - 1) {
+            ans++;
+            ind = nums[ind];
         }
-        return jumpCount;
+        return ans;
     }
 }
+
---
+++ spring/chad darby tutorial/AOP/readME.md
@@ -0,0 +1,44 @@
+
+I have a pretty good introduction about AOP in crud_apis. Please do refer to it.
+
+# AOP Terminology:
+
+- Aspect: Module of code for a cross cutting concer (logging, security)
+- Advice: What action is taken and when it should be applied
+- Join point: When to apply code during program execution
+- Pointcut: A predicate expression for where the advice should be applied
+
+# Advice Types
+
+- Before advice: run before the method
+- After finally advice: run after the method (finally)
+- After returning advice: run after the method (success execution)
+- After throwing advice: run after method (if exception thrown)
+- Around advice: run before and after method
+
+# Weaving
+
+Connecting aspects to target objects to create an advised object
+
+- Different types of weaving
+    - Compile time, load time or run time
+
+Performance is the lowest with run time weaving
+
+# AspectJ
+
+Very first AOP framework that was released. Enables full support for all AOP advice types, pointcuts etc.
+
+# Spring AOP
+
+Spring uses run time weaving to advise objects!
+
+You can convert them to AspectJ using @Aspect annotation
+
+Only supports `Method-level` join points
+
+# Project setup
+
+When starting a new project - select dependency `spring-boot-starter-aop`
+
+If you want to add support for legacy project `@EnableAspectJAutoProxy`
